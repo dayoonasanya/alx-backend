67a1a07a6545b3b29162f2f1c8c0536fd9db8a49
dayoonasanya <aonasanya@mail.dccc.edu>
2024-07-30 21:05:29 +0000

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,46 +1,43 @@
 #!/usr/bin/env python3
-"""A simple flask app
+"""
+Parametrize templates
 """
 
-
+import babel
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
+babel = Babel(app)
 
-class Config(object):
-    """_summary_
 
-    Returns:
-            _type_: _description_
+class Config:
+    """
+    Config class
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# configure the flask app
-app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
-babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """_summary_
-
-    Returns:
-            _type_: _description_
+    """
+     determine the best match with our supported languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """_summary_
+    """
+    hello world
     """
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(debug=True)
